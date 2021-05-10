453282712066e6c139285889eb23db6e6573a684
NookWarit <>
2021-05-10 10:25:14 +0700

    add pod spec

--- testttttttt/ViewController.swift
+++ testttttttt/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+//import Toast_Swift
 
 class ViewController: UIViewController {
 
@@ -23,7 +24,19 @@ class ViewController: UIViewController {
         let tableVC = self.storyboard?.instantiateViewController(withIdentifier: "TableDynamicViewController") as! TableDynamicViewController
         self.navigationController?.pushViewController(tableVC, animated: true)
     }
-
-
+    
+    @IBAction func toast_didtab() {
+//        self.view.makeToast("This is a piece of toast", duration: 3.0, position: .top)
+//        self.view.makeToast("noooooo no no no", duration: 3.0, point: CGPoint(x: 50.0, y: 50.0), title: "", image: UIImage(named: "wrong"), completion: {_ in
+//
+//        })
+//        showToastSuccess(message: "ไม่สามารถส่งได้ หรือส่งได้ ไม่รู้โว้ย")
+        showToastErrorWithButton(message: "ไม่สามารถส่ง log ได้นะ ", button: "ลองใหม่", clicked: { [ weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.toast_didtab()
+        })
+        
+        
+    }
 }
 
